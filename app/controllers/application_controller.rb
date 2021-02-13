6059cb7bfc29015e5ebb8d6eeb29d35d7d28f3d8
oopanpan <oopanpan@gmail.com>
2021-02-12 17:54:26 -0800

    not pround of it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #helper_method opens up method defined in application_controller to be available to the views
+  helper_method :cart
+
+  #method to assign an empty hash as the cart if not exist
+  def cart
+    session[:cart] ||= []
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  #I hate custom routes
+  get '/products', to: "products#index"
+
+  post '/products', to: "products#add", as: :add_product
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
